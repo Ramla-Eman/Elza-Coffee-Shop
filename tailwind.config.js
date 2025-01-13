77c76e502a6576e63ae1d803be17fd0a4ba7379b
Ramla-Razykan <ramla.eman.ra@gmail.com>
2025-01-14 01:58:10 +0500

    Hero section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       backgroundImage: {
         'navLinksActive': 'linear-gradient(to right, #DFC39D, #F8E4BE, #DFC39D, #F8E4BE, #F8E4BE)',
       },
-      fontFamily:{
+      fontFamily: {
         'Leiko': ['Lora', 'serif'], // Leiko font family 
         'DancingScript': ['Dancing Script', 'serif'], //Dancing script font family
       }
