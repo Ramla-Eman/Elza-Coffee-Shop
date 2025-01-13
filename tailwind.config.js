d158d9a145b6985959453a3e3cdb00e08ca9c155
Ramla-Razykan <ramla.eman.ra@gmail.com>
2025-01-14 02:26:45 +0500

    change styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'navLinksActive': 'linear-gradient(to right, #DFC39D, #F8E4BE, #DFC39D, #F8E4BE, #F8E4BE)',
+        'gradient-border': 'linear-gradient(to right, #DFC39D, #F8E4BE, #DFC39D, #F8E4BE, #F8E4BE)'
       },
       fontFamily: {
         'Leiko': ['Lora', 'serif'], // Leiko font family 
@@ -12,6 +13,17 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.gradient-border': {
+          'border-width': '4px',
+          'border-style': 'solid',
+          'background-clip': 'border-box',
+          'border-color': 'transparent',
+        },
+      });
+    },
+  ],
 }
 
